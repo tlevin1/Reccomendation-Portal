fa71125fe368b2cf22cd25151d9270b5b831f86a
jaltman-1 <jaltman1@umbc.edu>
2021-03-13 17:47:52 -0500

    Added basic tests for database table

--- src/LOR/tests.py
+++ src/LOR/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import LOR
+
 
 # Create your tests here.
+class LORModelTest(TestCase):
+    def test_to_string(self):
+        self.fail("Test Incomplete")
+
+    def test_good_to_string(self):
+        lor = LOR(requester="j@umbc.edu", writer="k@umbc.edu", status="pending", due_date="2021-01-01",
+                  position="developer")
+        self.assertEqual(str(lor), "j@umbc.edu k@umbc.edu pending 2021-01-01")
