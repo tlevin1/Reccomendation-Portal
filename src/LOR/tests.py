fd929e87ce33d3556246a302de8ac8519e508a64
jaltman-1 <jaltman1@umbc.edu>
2021-03-14 16:03:30 -0400

    Updated field names to match changes in database

--- src/LOR/tests.py
+++ src/LOR/tests.py
@@ -8,6 +8,6 @@ class LORModelTest(TestCase):
         self.fail("Test Incomplete")
 
     def test_good_to_string(self):
-        lor = LOR(requester="j@umbc.edu", writer="k@umbc.edu", status="pending", due_date="2021-01-01",
+        lor = LOR(requester_email="j@umbc.edu", writer_email="k@umbc.edu", status="pending", due_date="2021-01-01",
                   position="developer")
         self.assertEqual(str(lor), "j@umbc.edu k@umbc.edu pending 2021-01-01")
