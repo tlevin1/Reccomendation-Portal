3832e3aefd581207e2a50e652dc604e1dc94656f
qdang9674 <70727169+qdang9674@users.noreply.github.com>
2021-03-29 23:49:47 -0400

    update test code in write_name from k@gmail to kate

--- src/LOR/test/test_models.py
+++ src/LOR/test/test_models.py
@@ -7,15 +7,15 @@ class LORModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         RequestModel.objects.create(requester='Bob', requester_email='b@gmail.com', request_date='2021-01-01',
-                           position='developer', due_date='2021-02-02', writer_name='k@gmail.com',
+                           position='developer', due_date='2021-02-02', writer_name='kate',
                            company_name='NSA', company_website='nsa.gov', company_email='nsa@gov.com',
                            company_recipients='julia', status='pending', cv='TBD', resume='TBD',
                            transcript='TBD', additional_info='TBD')
 
     def test_good_to_string(self):
-        lor = RequestModel(requester="Bob", writer_name="k@umbc.edu", status="pending", due_date="2021-01-01",
+        lor = RequestModel(requester="Bob", writer_name="kate", status="pending", due_date="2021-01-01",
                   position="developer")
-        self.assertEqual(str(lor), "Bob k@umbc.edu pending 2021-01-01")
+        self.assertEqual(str(lor), "Bob kate pending 2021-01-01")
 
     def test_requester_label(self):
         lor = RequestModel.objects.get(id=1)
--- src/LOR/test/test_view.py
+++ src/LOR/test/test_view.py
@@ -9,7 +9,7 @@ class LOR_test_view(TestCase):
         self.client = Client()
         self.requesting_url = reverse('requesting')
         self.object1 = models.RequestModel.objects.create(requester='Bob', requester_email='b@gmail.com', request_date='2021-01-01',
-                           position='developer', due_date='2021-02-02', writer_name='k@gmail.com',
+                           position='developer', due_date='2021-02-02', writer_name='kate',
                            company_name='NSA', company_website='nsa.gov', company_email='nsa@gov.com',
                            company_recipients='julia', status='New', cv='TBD', resume='TBD',
                            transcript='TBD', additional_info='TBD')
@@ -27,7 +27,7 @@ class LOR_test_view(TestCase):
             'requester_email': 'b@gmail.com',
             'position': 'developer',
             'due_date': '2021-02-02',
-            'writer_name': 'k@gmail.com',
+            'writer_name': 'kate',
             'company_name': 'NSA',
             'company_website': 'nsa.gov',
             'company_email': 'nsa@gov.com',
@@ -44,7 +44,7 @@ class LOR_test_view(TestCase):
         self.assertEqual(self.object1.requester_email, "b@gmail.com")
         self.assertEqual(self.object1.position, "developer")
         self.assertEqual(self.object1.due_date, "2021-02-02")
-        self.assertEqual(self.object1.writer_name, "k@gmail.com")
+        self.assertEqual(self.object1.writer_name, "kate")
         self.assertEqual(self.object1.company_name, "NSA")
         self.assertEqual(self.object1.company_website, "nsa.gov")
         self.assertEqual(self.object1.company_recipients, "julia")
