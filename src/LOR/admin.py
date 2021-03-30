da88f9afacc36090ef0a5686613d1f45320c1857
Edwin Trejo <etrejo1@umbc.edu>
2021-03-30 19:32:25 -0400

    move to a different app

--- src/LOR/admin.py
+++ src/LOR/admin.py
@@ -4,7 +4,3 @@ from .models import *
 
 # Register your models here.
 admin.site.register(LOR)
-admin.site.register(LorUser)
-admin.site.register(LorRequest)
-admin.site.register(LorCompanyRecipient)
-admin.site.register(LorDocument)
