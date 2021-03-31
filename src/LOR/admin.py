cb3f99bb40eabdf391675f44149032d0d8cd41bd
jaltman-1 <jaltman1@umbc.edu>
2021-03-31 17:38:26 -0400

    Added LOR model back into models.py and included it in admin.py again so that everyone using the LOR table can still use it. Changed test_models to work with LOR again.

--- src/LOR/admin.py
+++ src/LOR/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import LOR
+from .models import RequestModel, LOR
 
 # Register your models here.
 admin.site.register(LOR)
+admin.site.register(RequestModel)
