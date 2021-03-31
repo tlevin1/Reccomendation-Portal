15be67be9e7aaf7611a3f427d10b0f877982de71
Edwin Trejo <etrejo1@umbc.edu>
2021-03-30 22:34:46 -0400

    Admin is able to change the role of a user

--- src/Authentication/admin.py
+++ src/Authentication/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.models import User
 
+#add sample users
+
 # Register your models here.
 admin.site.register(LorUser)
 admin.site.register(LorRequest)
--- src/Authentication/views.py
+++ src/Authentication/views.py
@@ -1,4 +1,5 @@
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import LorUser, UserRoles
@@ -8,8 +9,17 @@ from django.shortcuts import render, redirect
 
 
 # Create your views here.
+'''
 @staff_member_required
 def index(request):
-    if (LorUser.objects.filter(base_user=request.user) is None):
-        LorUser.objects.Create(request.user, first_name=request.user.username, role=UserRoles.ADMIN)
+    if (LorUser.objects.filter(base_user=request.user).first() is None):
+        LorUser.objects.create(base_user=request.user, first_name=request.user.username, role=UserRoles.ADMIN)
+    return HttpResponse('auth screen')
+'''
+
+
+@login_required
+def index(request):
+    if (LorUser.objects.filter(base_user=request.user).first() is None):
+        LorUser.objects.create(base_user=request.user, first_name=request.user.username, role=UserRoles.REQUESTER)
     return HttpResponse('auth screen')
