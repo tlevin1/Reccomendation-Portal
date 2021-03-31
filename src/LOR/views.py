6624fcf51b2b8cc7dbd33b435da6f4f2e96fac4c
jaltman-1 <jaltman1@umbc.edu>
2021-03-31 19:06:49 -0400

    Added a better way to ensure user is logged in to view writer dashboard.

--- src/LOR/views.py
+++ src/LOR/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import LOR
 
@@ -10,6 +11,7 @@ def index(request):
 
 # display table of writer actions
 # actions are: accept, deny, review, complete
+@login_required
 def writer_view(request):
     print(request.method)
     cur_user = request.user
@@ -17,27 +19,27 @@ def writer_view(request):
     print("User is ", cur_user)
 
     # Make sure user is logged in
-    if cur_user.is_authenticated:
-        print("User is logged in")
-        if request.method == 'POST':
-            # Get the list of ids associated with selected table rows
-            if not request.POST.getlist('sel_box'):
-                # messages.info(request, 'Nothing Selected')
-                print('Nothing selected')
-            else:
-                sel_ids = request.POST.getlist('sel_box')
-                # Check which button is pressed
-                if 'Accept' in request.POST:
-                    print('Accept Button pressed')
-                elif 'Deny' in request.POST:
-                    print('Deny Button pressed')
-                elif 'Complete' in request.POST:
-                    print('Complete Button pressed')
-                elif 'Review' in request.POST:
-                    print('Review Button pressed')
-        sorted_lors = LOR.objects.filter(writer_email=cur_user.email).order_by("due_date")
-        context = {"sorted_lors": sorted_lors}
-        return render(request, 'writer_view.html', context)
-    else:
-        # If no one is logged in, send back to home page
-        return redirect('/')
+    # if cur_user.is_authenticated:
+    print("User is logged in")
+    if request.method == 'POST':
+        # Get the list of ids associated with selected table rows
+        if not request.POST.getlist('sel_box'):
+            # messages.info(request, 'Nothing Selected')
+            print('Nothing selected')
+        else:
+            sel_ids = request.POST.getlist('sel_box')
+            # Check which button is pressed
+            if 'Accept' in request.POST:
+                print('Accept Button pressed')
+            elif 'Deny' in request.POST:
+                print('Deny Button pressed')
+            elif 'Complete' in request.POST:
+                print('Complete Button pressed')
+            elif 'Review' in request.POST:
+                print('Review Button pressed')
+    sorted_lors = LOR.objects.filter(writer_email=cur_user.email).order_by("due_date")
+    context = {"sorted_lors": sorted_lors}
+    return render(request, 'writer_view.html', context)
+# else:
+    # If no one is logged in, send back to home page
+    # return redirect('/')
