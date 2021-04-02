e754488f2bfcd47ee3748f9437805e928b61b4c5
tehila <tlevin1@umbc.edu>
2021-04-02 16:37:18 -0400

    created writer_review.html for letter writer to review requests

--- src/LOR/views.py
+++ src/LOR/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
-from django.contrib.auth.decorators import login_required
-
-from django import http
-
-import models
+from . import models
 
 
 
