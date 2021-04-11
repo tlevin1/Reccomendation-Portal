9b22e268a77d091ec7e74597f89a03ccafd4bb37
tehila <tlevin1@umbc.edu>
2021-04-11 19:26:26 -0400

    added url for requester view

--- src/LOR/urls.py
+++ src/LOR/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('request/', views.writer_req, name="writer_req"),
     #path('save/', views.view_save_request, name='save'),
 path('writer_rev/', views.writer_review, name="writer_review"),
+path('req_view/', views.requester_view, name="requester_view")
 ]
