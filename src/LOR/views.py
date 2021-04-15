499272d516e40336243dfcb26ebbd37406ffa393
tehila <tlevin1@umbc.edu>
2021-04-15 17:50:59 -0400

    combined urls.py with develop

--- src/LOR/views.py
+++ src/LOR/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import LOR, Req_a,UpdateRequest
+from .models import LOR, Req_a
 from datetime import datetime
 from django import http
 from . import models
@@ -10,31 +11,38 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
-
-
-
 # stub for home page
 def index(request):
     return HttpResponse('Letters of Recommendation index')
 
 
-# display table of writer actions
+# Change request status to given new status if current status isn't in invalid_sel list
+def change_status(sel_ids, invalid_sel, new_status):
+    error = False
+    for id in sel_ids:
+        lor = LOR.objects.get(id=id)
+        if lor.status != new_status:
+            if lor.status in invalid_sel:
+                error = True
+            else:
+                lor.status = new_status
+                lor.save()
+    return error
+
+
+# display writer dashboard
 # actions are: accept, deny, review, complete
 @login_required
 def writer_view(request):
     print(request.method)
     cur_user = request.user
-    print(cur_user)
-    print("User is ", cur_user)
+    print("Logged in user is ", cur_user)
 
-    # Make sure user is logged in
-    # if cur_user.is_authenticated:
-    print("User is logged in")
     if request.method == 'POST':
         # Get the list of ids associated with selected table rows
         if not request.POST.getlist('sel_box'):
-            # messages.info(request, 'Nothing Selected')
-            print('Nothing selected')
+            messages.info(request, 'Nothing Selected')
+            # print('Nothing selected')
         else:
             sel_ids = request.POST.getlist('sel_box')
             # Check which button is pressed
@@ -46,14 +54,56 @@ def writer_view(request):
                 print('Complete Button pressed')
             elif 'Review' in request.POST:
                 print('Review Button pressed')
+
+    # for POST or GET, get logged in writer's requests sorted by due date
     sorted_lors = LOR.objects.filter(writer_email=cur_user.email).order_by("due_date")
     context = {"sorted_lors": sorted_lors}
     return render(request, 'writer_view.html', context)
-# else:
-    # If no one is logged in, send back to home page
-    # return redirect('/')
 
-# Create your views here.
+
+# display requester dashboard
+# actions are: New Request, Review, Withdraw
+@login_required
+def requester_view(request):
+    print(request.method)
+    cur_user = request.user
+    print("Logged in user is ", cur_user)
+
+    if request.method == 'POST':
+        # Get the list of ids associated with selected table rows
+        if not request.POST.getlist('sel_box'):
+            messages.info(request, 'Nothing selected')
+            # print('Nothing selected')
+        else:
+            sel_ids = request.POST.getlist('sel_box')
+            # Check which button is pressed
+            if 'New Request' in request.POST:
+                print('New Request Button pressed')
+            elif 'Review' in request.POST:
+                print('Review Button pressed')
+            elif 'Withdraw' in request.POST:
+                # change status to withdrawn if status is not already completed
+                print('Withdraw Button pressed')
+                new_status = 'Withdrawn'
+                invalid_sel = {'Completed'}
+                errmsg = 'Unable to withdraw a completed request'
+                if change_status(sel_ids, invalid_sel, new_status):
+                    messages.info(request, errmsg)
+
+    # for POST or GET, get logged in user's requests sorted by due date
+    sorted_lors = LOR.objects.filter(requester_email=cur_user.email).order_by("due_date")
+    context = {"sorted_lors": sorted_lors}
+    return render(request, 'requester_view.html', context)
+
+'''
+def view_enter_request(request):
+    obj = models.Request_Form.objects.get(id=1)
+    request_form = {
+        "object" : obj
+    }
+    return render(request,"LOR/enter_request.html", request_form);
+'''
+
 
 def view_enter_request(request):
     obj = form.RequestForm(request.POST or None)
@@ -64,7 +114,8 @@ def view_enter_request(request):
     request_form = {
         "object": obj
     }
-    return render(request,"LOR/enter_request.html", request_form);
+    return render(request,"LOR/enter_request.html", request_form)
+
 
 def writer_req(request):
     #form = ReqForm()
@@ -80,6 +131,7 @@ def writer_req(request):
         #save into database
 
     return render(request, 'LOR/requests.html', {'answers': answer})
+
 
 def writer_review(request):
     #all data
@@ -112,11 +164,11 @@ def requester_review(request):
     print(cur_user)
     print("User is ", cur_user)
 
-    # Make sure user is logged in
-    # if cur_user.is_authenticated:
+# Make sure user is logged in
+# if cur_user.is_authenticated:
     print("User is logged in")
-    # Check which button is pressed
-    #POST - result of form submission
+# Check which button is pressed
+#POST - result of form submission
     if request.method == 'POST':
         if not request.POST.getlist('sel_box'):
             #messages.info(request, 'Nothing Selected')
@@ -131,7 +183,6 @@ def requester_review(request):
             print('Withdraw Button pressed')
         else:
             sel_ids = request.POST.getlist('sel_box')
-
 
     return render(request, 'LOR/requester_review.html', {'objs': obj})
 
