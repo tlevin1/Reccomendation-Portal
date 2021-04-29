8fd545d9065b94cc948a3e9b9d07ed5c44b83000
Edwin Trejo <etrejo1@umbc.edu>
2021-04-29 12:22:11 -0400

    add index auth

--- src/LOR/urls.py
+++ src/LOR/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.conf.urls import include, url
 from . import views
+from django.views.generic import TemplateView
+from Authentication import views as authviews
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('writer/', views.writer_view, name="writer_view"),
@@ -10,6 +14,7 @@ urlpatterns = [
     path('writer_rev/', views.writer_review, name="writer_review"),
   path('req_view/', views.requester_review, name="requester_review"),
   path('req_page', views.requester_view_particular_request, name="request_page"),
+  path('Authentication', TemplateView.as_view(template_name="Authentication/index.html"), name='Authentication'),
 ]
 
 
